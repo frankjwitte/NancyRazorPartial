7281465c4fd576867df1ac11ddd7db84837ae630
frankjwitte <frank@franksworld.nl>
2013-11-20 10:06:31 +0100

    Initial commit

---
+++ Modules/DefaultModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NancyRazorPartial.Modules
+{
+    public class DefaultModule : Nancy.NancyModule
+    {
+        public DefaultModule()
+        {
+            Get["/"] = _ =>
+            {
+                return View["Index"];
+            };
+        }
+    }
+}
